11318132012316bd77541c3ccee44a308dd72030
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-03-19 01:34:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/XcuteboyX/Trying-best")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv("GIT_TOKEN", None)
+GIT_TOKEN = getenv("GIT_TOKEN", "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")
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/the_update_silencer")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/x_team_immortals_x")
