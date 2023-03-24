d8976fe9475bd0a8a3c3a44cd698c41f37fc0588
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-03-24 23:26:57 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -37,6 +37,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         [
             InlineKeyboardButton(
                 text="✷ ʜᴇʟᴩ ✷", callback_data="settings_back_helper"
+            )
         ],
         [    
             InlineKeyboardButton(
