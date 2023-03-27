1d75ce1912ca4eb4a76b22edad0136f4d840524a
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-03-27 13:20:38 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -216,8 +216,8 @@ async def start_comm(client, message: Message, _):
                 SD = random.choice(CHAMPU)
                 AK = random.choice(ABHI)
                 await message.reply_sticker(sticker=AK)
-                await message.reply_url(
-                    url=SD,
+                await message.reply_photo(
+                    Photo=SD,
                     caption=_["start_2"].format(
                         config.MUSIC_BOT_NAME
                     ),
