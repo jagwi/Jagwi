854833054cff5ddf817cd8cc57d7bbbd7ab50178
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-03-30 00:43:29 +0530

    Add files via upload

---
+++ AnonX/plugins/owner.py
@@ -0,0 +1,57 @@
+from pyrogram import Client, filters
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from AnonX import app 
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton 
+
+
+
+@app.on_message(
+    filters.command("repo")
+    & filters.private
+    & ~filters.edited & filters.private & ~filters.edited)
+async def help(client: Client, message: Message):
+    
+@pbot.on_message(filters.command("repo"))
+async def restart(client, m: Message):
+    await m.delete()
+    accha = await m.reply("⚡")
+    await asyncio.sleep(0.5)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ..")
+    await asyncio.sleep(0.5)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ......")
+    await asyncio.sleep(0.5)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ..")
+    await asyncio.sleep(0.5)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ......")
+    await accha.delete()
+    await asyncio.sleep(0.5)
+    umm = await message.reply_sticker(
+
+"CAACAgUAAxkBAAIDG2QhN85PjxC3IZl3hYefSbz_w60-AAI-CQAC5Nr5V3U6V4xWQpckLwQ")
+    await umm.delete()
+    await asyncio.sleep(2)
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/4b52da6d880cbb199298a.jpg",
+        caption=f"""ʜᴇʏ ʙᴀʙʏ🖤\n\nʜᴇʀᴇ ɪs ʙᴏᴛ ʀᴇᴘᴏ 💞""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC")
+                ]
+            ]
+        ),
+    )
+
+
+
+
+
+
+
