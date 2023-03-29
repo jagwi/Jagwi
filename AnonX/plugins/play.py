63aa7d7c62ee1aa71d638c79e51722bf51308cc7
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-03-29 13:17:38 +0530

    Update play.py

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -49,7 +49,7 @@ async def play_commnd(
     url,
     fplay,
 ):
-    mystic = await message.reply_text(
+    mystic = await message.reply_photo(photo=config.START_IMG_URL,
         _["play_2"].format(channel) if channel else _["play_1"]
     )
     plist_id = None
