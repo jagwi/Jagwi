d810280c236e848252ff481ba3fc44263cf67329
jagwi <128573004+jagwi@users.noreply.github.com>
2023-04-23 11:11:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,9 +21,9 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5673255098").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/XcuteboyX/TRYING-BEST-2")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/jagwi/Jagwi")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv("GIT_TOKEN", "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")
+GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/the_update_silencer")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/x_team_immortals_x")
